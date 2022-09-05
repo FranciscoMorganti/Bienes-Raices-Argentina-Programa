bc245fd4450e23f27ac979a17e89196b0877e0af
Francisco Morganti <franciscomorganti45@gmail.com>
2022-09-05 16:06:15 -0300

    Trabajo terminado, vamo a jugar

---
+++ script.js
@@ -0,0 +1,5 @@
+const boton= document.querySelector('.boton');
+
+boton.addEventListener('click', () => {
+    alert('Desde bienes raices lamentamos informarle que hasta aca llego el presupuesto para la pagina web, no hay mas nada por ahora');
+});
